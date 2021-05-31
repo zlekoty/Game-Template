ee0aee512dd2bc17fc2e88ad50975c41d1ee331e
Joanna Kołaczek <262338@student.pwr.edu.pl>
2021-05-31 23:31:57 +0200

    zycia i score

--- 1gra/wolf&chickencops.py
+++ 1gra/wolf&chickencops.py
@@ -9,6 +9,16 @@ FPS=50
 bg = pygame.image.load('obrazki/Tlo.jpg')
 background=pygame.transform.scale(bg,(WIDTH,HEIGHT))
 void=pygame.image.load('obrazki/nic.png')
+
+    #lives
+l3=pygame.image.load('obrazki/3lives.png')
+l2=pygame.image.load('obrazki/2lives.png')
+l1=pygame.image.load('obrazki/1lives.png')
+
+lives_3=pygame.transform.scale(l3,(235,95))
+lives_2=pygame.transform.scale(l2,(235,95))
+lives_1=pygame.transform.scale(l1,(235,95))
+
     #wolf
 UL=pygame.image.load('obrazki/LG.png')
 UR=pygame.image.load('obrazki/PG.png')
@@ -33,10 +43,19 @@ egg_3=pygame.transform.scale(e3,(60,60))
 egg_4=pygame.transform.scale(e4,(60,60))
 egg_5=pygame.transform.scale(e5,(60,60))
 
-#FUNCTIONS
+#PARAMETERS
 n=250
 m=250
+score=0
+lives=3
+skip=[0]
 
+#FUNCTIONS
+def points(score):
+    font = pygame.font.SysFont("comicsansms", 41)
+    text=font.render(str(score), True, (0,0,0))
+    screen.blit(text,(440,7))
+    
 #CLASSES
 class Wolf(pygame.sprite.Sprite):
     def __init__(self):
@@ -58,6 +77,28 @@ class Wolf(pygame.sprite.Sprite):
         if keys[pygame.K_l]:
             self.image=wolf_DR
             self.rect.center=(450,300)
+
+class PhantomWolf(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=void
+        self.rect=self.image.get_rect()
+        self.rect.center=(WIDTH/2,HEIGHT/2)
+    def update(self):
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            self.image=void
+            self.rect.center=(0,0)
+        if keys[pygame.K_a]:
+            self.image=void
+            self.rect.center=(0,HEIGHT)
+        if keys[pygame.K_p]:
+            self.image=void
+            self.rect.center=(WIDTH,0)
+        if keys[pygame.K_l]:
+            self.image=void
+            self.rect.center=(WIDTH,HEIGHT)
+        
             
 class Egg_pos1(pygame.sprite.Sprite):
     def __init__(self):
@@ -70,21 +111,25 @@ class Egg_pos1(pygame.sprite.Sprite):
         self.updated_UL2=False
         self.updated_UL3=False
         self.updated_UL4=False
+        self.updated_UL_phantom=False
 
         self.updated_DL1=False
         self.updated_DL2=False
         self.updated_DL3=False
         self.updated_DL4=False
+        self.updated_DL_phantom=False
 
         self.updated_UR1=False
         self.updated_UR2=False
         self.updated_UR3=False
         self.updated_UR4=False
+        self.updated_UR_phantom=False
 
         self.updated_DR1=False
         self.updated_DR2=False
         self.updated_DR3=False
         self.updated_DR4=False
+        self.updated_DR_phantom=False
         
     def update(self):
         #upper-left egg
@@ -115,6 +160,12 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_5
             self.rect.center=(275,220)
             self.updated_UL4=False
+            self.updated_UL_phantom=True
+
+        if self.updated_UL_phantom==True and m==1:
+            self.image=void
+            self.rect.center=(0,0)
+            self.updated_UL_phantom=False
             
         #down-left egg
         if first_pos==2:
@@ -144,6 +195,12 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_5
             self.rect.center=(260,320)
             self.updated_DL4=False
+            self.updated_DL_phantom=True
+
+        if self.updated_DL_phantom==True and m==1:
+            self.image=void
+            self.rect.center=(0,HEIGHT)
+            self.updated_DL_phantom=False
             
         #upper-right egg
         if first_pos==3:
@@ -173,6 +230,12 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_5
             self.rect.center=(575,195)
             self.updated_UR4=False
+            self.updated_UR_phantom=True
+            
+        if self.updated_UR_phantom==True and m==1:
+            self.image=void
+            self.rect.center=(WIDTH,0)
+            self.updated_UR_phantom=False
 
         #down-right egg
         if first_pos==4:
@@ -202,11 +265,17 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_5
             self.rect.center=(550,305)
             self.updated_DR4=False
+            self.updated_DR_phantom=True
+            
+        if self.updated_DR_phantom==True and m==1:
+            self.image=void
+            self.rect.center=(WIDTH,HEIGHT)
+            self.updated_DR_phantom=False
 
         #empty
         if first_pos==5:
             self.image=void
-
+            self.rect.center=(WIDTH/2,HEIGHT)
 
 
 
@@ -221,8 +290,9 @@ all_sprites= pygame.sprite.Group()
 eggs=pygame.sprite.Group()
 
 wolf=Wolf()
+phantomwolf=PhantomWolf()
 all_sprites.add(wolf)
-
+all_sprites.add(phantomwolf)
 
 eggs.add(Egg_pos1())
 
@@ -240,8 +310,9 @@ while running:
         first_pos=0
     else:
         n=250
-        first_pos=random.choice([1,2,3,4])
-        print(first_pos)
+        first_pos=random.choice([1,2,3,4,5,5])
+        skip[0]=first_pos
+        skip=skip.copy()
         
     #timer2
     if m>1:
@@ -253,9 +324,23 @@ while running:
     all_sprites.update()
     eggs.update()
     
-
+    #score&lives
+    if pygame.sprite.spritecollide(phantomwolf,eggs,False):
+        score+=1
+    if m==1 and not pygame.sprite.spritecollide(phantomwolf,eggs,False) and skip[0]!=5:
+        lives-=1
+    if lives==2:
+        screen.blit(lives_3,(390,475))
+    if lives==1:
+        screen.blit(lives_2,(390,475))
+    if lives==0:
+        screen.blit(lives_1,(390,475))
+    if lives==-1:
+        print('loose')
+        
     all_sprites.draw(screen)
     eggs.draw(screen)
+    points(score)
 
 
     pygame.display.update()
