2632ff2db1ffa111fd3f09da131b35e9abc22c51
Joanna Kołaczek <262338@student.pwr.edu.pl>
2021-06-06 22:14:42 +0200

    dzwięki i highscores

--- 1gra/wolf&chickencops.py
+++ 1gra/wolf&chickencops.py
@@ -2,25 +2,33 @@ import pygame
 import random
 
 WIDTH=800
-HEIGHT=574
+HEIGHT=571
 FPS=50
 
 #IMAGES
-    #menu
-mbg=pygame.image.load('obrazki/tlomenu.jpg')
-menubackground=pygame.transform.scale(mbg,(WIDTH,HEIGHT))
-gt=pygame.image.load('obrazki/menutitle.png')
-game_title=pygame.transform.scale(gt,(WIDTH,HEIGHT))
+
+    #menu background
+menubackground=pygame.image.load('obrazki/tlomenu.jpg')
+game_title=pygame.image.load('obrazki/menutitle.png')
+
+    #buttons
 play_button=pygame.image.load('obrazki/buttonplay.png')
 highscores_button=pygame.image.load('obrazki/buttonhighscores.png')
 about_button=pygame.image.load('obrazki/buttonabout.png')
-about_info=pygame.image.load('obrazki/about.png')
 back_button=pygame.image.load('obrazki/buttonback.png')
 
-    #game bg
+
+about_info=pygame.image.load('obrazki/about.png')
+highscores_info=pygame.image.load('obrazki/highscores.png')
+
+go=pygame.image.load('obrazki/gameover.jpg')
+gameover=pygame.transform.scale(go,(WIDTH,HEIGHT))
+
+void=pygame.image.load('obrazki/nic.png')
+
+    #game background
 bg = pygame.image.load('obrazki/Tlo.jpg')
 background=pygame.transform.scale(bg,(WIDTH,HEIGHT))
-void=pygame.image.load('obrazki/nic.png')
 
     #lives
 l3=pygame.image.load('obrazki/3lives.png')
@@ -45,7 +53,7 @@ wolf_DR=pygame.transform.scale(DR,(275,275))
     #eggs
 e1=pygame.image.load('obrazki/UL_1.png')
 e2=pygame.image.load('obrazki/UL_2.png')
-e3=pygame.image.load('obrazki/UL_3.png')
+e3=pygame.image.load('obrazki/UR_3.png')
 e4=pygame.image.load('obrazki/UL_4.png')
 e5=pygame.image.load('obrazki/UL_5.png')
 
@@ -54,6 +62,10 @@ egg_2=pygame.transform.scale(e2,(60,60))
 egg_3=pygame.transform.scale(e3,(60,60))
 egg_4=pygame.transform.scale(e4,(60,60))
 egg_5=pygame.transform.scale(e5,(60,60))
+
+#MUSIC
+generate_soundtrack=random.choice(['muza/music1.mp3','muza/music2.mp3','muza/music3.mp3','muza/music4.mp3'])
+
 
 #PARAMETERS
 i=0
@@ -68,14 +80,34 @@ n_mod=int(n_copied/5)
     
 score=0
 lives=3
-skip=[0]
 first_pos=0
 
+end_game=False
+
+
+name='Username: (press any key to add)'
+
+highscores_text=open('highscores.txt','r')
+highscores_lines=highscores_text.readlines()
+highscores_help=[]
+lines_help=[]
+highscores_text.close()
+
+for line in highscores_lines:
+    try:
+        a=line.split('...')
+        a[0]=int(a[0])
+        highscores_help.append(a)
+    except:
+        pass
+print(highscores_help)
+
 #FUNCTIONS
-def points(score):
+def points(score,colour,place):
     font = pygame.font.SysFont("comicsansms", 41)
-    text=font.render(str(score), True, (0,0,0))
-    screen.blit(text,(440,7))
+    text=font.render(str(score), True, colour)
+    screen.blit(text,place)
+
     
 #CLASSES
 class Wolf(pygame.sprite.Sprite):
@@ -84,6 +116,7 @@ class Wolf(pygame.sprite.Sprite):
         self.image=void
         self.rect=self.image.get_rect()
         self.rect.center=(WIDTH/2,HEIGHT/2)
+        
     def update(self):
         keys=pygame.key.get_pressed()
         if keys[pygame.K_q]:
@@ -102,6 +135,11 @@ class Wolf(pygame.sprite.Sprite):
             self.image=wolf_DR
             self.rect=self.image.get_rect()
             self.rect.center=(450,300)
+        if end_game:
+            self.image=void
+            self.rect=self.image.get_rect()
+            self.rect.center=(WIDTH/2,HEIGHT/2)
+            
 
 class PhantomWolf(pygame.sprite.Sprite):
     def __init__(self):
@@ -123,9 +161,13 @@ class PhantomWolf(pygame.sprite.Sprite):
         if keys[pygame.K_l]:
             self.image=void
             self.rect.center=(WIDTH,HEIGHT)
+        if end_game:
+            self.image=void
+            self.rect=self.image.get_rect()
+            self.rect.center=(WIDTH/2,HEIGHT/2)
         
             
-class Egg_pos1(pygame.sprite.Sprite):
+class Eggs(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image=void
@@ -163,30 +205,35 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_1
             self.rect.center=(100,100)
             self.updated_UL1=True
+            egg_sound1.play()
 
         if self.updated_UL1==True and n==n_copied-1*n_mod:
             self.image=egg_2
             self.rect.center=(150,125)
             self.updated_UL1=False
             self.updated_UL2=True
+            egg_sound2.play()
             
         if self.updated_UL2==True and n==n_copied-2*n_mod:
             self.image=egg_3
             self.rect.center=(190,170)
             self.updated_UL2=False
             self.updated_UL3=True
+            egg_sound3.play()
             
         if self.updated_UL3==True and n==n_copied-3*n_mod:
             self.image=egg_4
             self.rect.center=(230,200)
             self.updated_UL3=False
             self.updated_UL4=True
+            egg_sound4.play()
             
         if self.updated_UL4==True and n==n_copied-4*n_mod:
             self.image=egg_5
             self.rect.center=(275,220)
             self.updated_UL4=False
             self.updated_UL_phantom=True
+            egg_sound5.play()
 
         if self.updated_UL_phantom==True and n==1:
             self.image=void
@@ -198,30 +245,35 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_1
             self.rect.center=(75,325)
             self.updated_DL1=True
+            egg_sound1.play()
             
         if self.updated_DL1==True and n==n_copied-1*n_mod:
             self.image=egg_2
             self.rect.center=(140,325)
             self.updated_DL1=False
             self.updated_DL2=True
+            egg_sound2.play()
             
         if self.updated_DL2==True and n==n_copied-2*n_mod:
             self.image=egg_3
             self.rect.center=(175,325)
             self.updated_DL2=False
             self.updated_DL3=True
+            egg_sound3.play()
             
         if self.updated_DL3==True and n==n_copied-3*n_mod:
             self.image=egg_4
             self.rect.center=(215,315)
             self.updated_DL3=False
             self.updated_DL4=True
+            egg_sound4.play()
             
         if self.updated_DL4==True and n==n_copied-4*n_mod:
             self.image=egg_5
             self.rect.center=(260,320)
             self.updated_DL4=False
             self.updated_DL_phantom=True
+            egg_sound5.play()
 
         if self.updated_DL_phantom==True and n==1:
             self.image=void
@@ -233,30 +285,35 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_2
             self.rect.center=(700,100)
             self.updated_UR1=True
+            egg_sound1.play()
 
         if self.updated_UR1==True and n==n_copied-1*n_mod:
             self.image=egg_1
             self.rect.center=(675,125)
             self.updated_UR1=False
             self.updated_UR2=True
+            egg_sound2.play()
             
         if self.updated_UR2==True and n==n_copied-2*n_mod:
             self.image=egg_3
             self.rect.center=(625,160)
             self.updated_UR2=False
             self.updated_UR3=True
+            egg_sound3.play()
             
         if self.updated_UR3==True and n==n_copied-3*n_mod:
             self.image=egg_4
             self.rect.center=(600,180)
             self.updated_UR3=False
             self.updated_UR4=True
+            egg_sound4.play()
             
         if self.updated_UR4==True and n==n_copied-4*n_mod:
             self.image=egg_5
             self.rect.center=(575,195)
             self.updated_UR4=False
             self.updated_UR_phantom=True
+            egg_sound5.play()
             
         if self.updated_UR_phantom==True and n==1:
             self.image=void
@@ -268,35 +325,70 @@ class Egg_pos1(pygame.sprite.Sprite):
             self.image=egg_1
             self.rect.center=(720,350)
             self.updated_DR1=True
+            egg_sound1.play()
             
         if self.updated_DR1==True and n==n_copied-1*n_mod:
             self.image=egg_2
             self.rect.center=(670,335)
             self.updated_DR1=False
             self.updated_DR2=True
+            egg_sound2.play()
             
         if self.updated_DR2==True and n==n_copied-2*n_mod:
             self.image=egg_3
             self.rect.center=(625,325)
             self.updated_DR2=False
             self.updated_DR3=True
+            egg_sound3.play()
             
         if self.updated_DR3==True and n==n_copied-3*n_mod:
             self.image=egg_4
             self.rect.center=(580,315)
             self.updated_DR3=False
             self.updated_DR4=True
+            egg_sound4.play()
             
         if self.updated_DR4==True and n==n_copied-4*n_mod:
             self.image=egg_5
             self.rect.center=(550,305)
             self.updated_DR4=False
             self.updated_DR_phantom=True
+            egg_sound5.play()
             
         if self.updated_DR_phantom==True and n==1:
             self.image=void
             self.rect.center=(WIDTH,HEIGHT)
             self.updated_DR_phantom=False
+
+        if end_game==True:
+            self.image=void
+            self.rect=self.image.get_rect()
+            self.rect.center=(100,100)
+
+            self.updated_UL1=False
+            self.updated_UL2=False
+            self.updated_UL3=False
+            self.updated_UL4=False
+            self.updated_UL_phantom=False
+
+            self.updated_DL1=False
+            self.updated_DL2=False
+            self.updated_DL3=False
+            self.updated_DL4=False
+            self.updated_DL_phantom=False
+
+            self.updated_UR1=False
+            self.updated_UR2=False
+            self.updated_UR3=False
+            self.updated_UR4=False
+            self.updated_UR_phantom=False
+
+            self.updated_DR1=False
+            self.updated_DR2=False
+            self.updated_DR3=False
+            self.updated_DR4=False
+            self.updated_DR_phantom=False
+            
             
 
 
@@ -308,6 +400,21 @@ screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Wolf & Chicken cops')
 clock=pygame.time.Clock()
 
+music=pygame.mixer.music.load(generate_soundtrack)
+pygame.mixer.music.set_volume(0.01)
+pygame.mixer.music.play(-1)
+
+beep=pygame.mixer.Sound('muza/cursor.wav')
+beep.set_volume(0.008)
+egg_sound1=pygame.mixer.Sound('muza/egg_sound1.wav')
+egg_sound2=pygame.mixer.Sound('muza/egg_sound2.wav')
+egg_sound3=pygame.mixer.Sound('muza/egg_sound3.wav')
+egg_sound4=pygame.mixer.Sound('muza/egg_sound4.wav')
+egg_sound5=pygame.mixer.Sound('muza/egg_sound5.wav')
+egg_cracking=pygame.mixer.Sound('muza/egg_cracking.wav')
+collect=pygame.mixer.Sound('muza/collect.wav')
+
+
 all_sprites= pygame.sprite.Group()
 eggs=pygame.sprite.Group()
 
@@ -316,9 +423,34 @@ phantomwolf=PhantomWolf()
 all_sprites.add(wolf)
 all_sprites.add(phantomwolf)
 
-eggs.add(Egg_pos1())
+eggs.add(Eggs())
 
+def game_over():
+
+    global score
+    
+    running=True
+    while running:
+        screen.blit(gameover,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_SPACE:
+                    running=False
+
+        points(score,(255,0,0),(540,280))
+
+        pygame.display.update()
+                
 def game_intro():
+    
+    global name
+    global highscores_help
+    global highscores_lines
+    
+    beep_play=0
+    k=0
+    
+    
     intro = True
     while intro:
         screen.blit(menubackground,(0,0))
@@ -327,6 +459,21 @@ def game_intro():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if k==0:
+                    name=name[:-22]
+                    k+=1
+                if event.key==pygame.K_BACKSPACE:
+                    if len(name)>10:
+                        name=name[:-1]
+                else:
+                    if len(name)<29:
+                        name+=event.unicode
+        
+        font = pygame.font.SysFont("comicsansms", 20)
+        text=font.render(name, True, (255,0,0))
+        screen.blit(text,(270,500))
+                
 
         screen.blit(play_button,(195,160))
         #pygame.draw.ellipse(screen,(0,0,0),(200,170,400,60))
@@ -334,7 +481,6 @@ def game_intro():
         #pygame.draw.ellipse(screen,(0,0,0),(220,250,360,60))
         screen.blit(about_button,(245,330))
         #pygame.draw.ellipse(screen,(0,0,0),(240,330,320,60))
-        #pygame.display.update()
 
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
@@ -342,36 +488,60 @@ def game_intro():
         if 200<mouse[0]<600 and 170<mouse[1]<230:
             pygame.draw.circle(screen,(0,0,0),(180,200),10)
             pygame.draw.circle(screen,(0,0,0),(630,200),10)
+            if beep_play==0:
+                beep.play()
+                beep_play=1
+            
             if click[0]==1:
+                
                 game_loop()
                 i=0
                 j=-1
                 n=125
-                speed=[]
-                for _ in range(23):
-                    speed.append(n)
-                    n-=5
                 n_copied=speed[i]
                 n_mod=int(n_copied/5)
                     
                 score=0
                 lives=3
-                skip=[0]
                 first_pos=0
-        if 220<mouse[0]<580 and 250<mouse[1]<310:
+                
+        elif 220<mouse[0]<580 and 250<mouse[1]<310:
             pygame.draw.circle(screen,(0,0,0),(200,280),10)
             pygame.draw.circle(screen,(0,0,0),(610,280),10)
-        if 240<mouse[0]<560 and 330<mouse[1]<390:
+            if beep_play==0:
+                beep.play()
+                beep_play=1
+            if click[0]==1:
+                game_highscores()
+            
+        elif 240<mouse[0]<560 and 330<mouse[1]<390:
             pygame.draw.circle(screen,(0,0,0),(225,360),10)
             pygame.draw.circle(screen,(0,0,0),(575,360),10)
+            if beep_play==0:
+                beep.play()
+                beep_play=1
             if click[0]==1:
                 game_about()
+        else:
+            beep_play=0
 
+        highscores_help=sorted(highscores_help,key=lambda sc: sc[0],reverse=True)
+        highscores_help=highscores_help[:10]
+        highscores_text=open('highscores.txt','w+')
+        
+        for hh in highscores_help:
+            new=str(hh[0])+'...'+hh[1]
+            highscores_text.write(new)
+        highscores_text.close()
+        
+        
         pygame.display.update()
                 
     
 def game_loop():
-
+    
+    global highscores_help
+    global name
     global first_pos
     global i
     global j
@@ -381,7 +551,9 @@ def game_loop():
     global n_mod
     global score
     global lives
-    global skip
+    global end_game
+    
+    beep_play=0
    
     running=True
     while running:
@@ -389,6 +561,13 @@ def game_loop():
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
+                highscores_help.append([score,name[10:]+'\n'])
+                i=0
+                j=-1
+                n=125
+                score=0
+                lives=3
+                first_pos=0
                 running=False
                 
         #timer1
@@ -413,8 +592,11 @@ def game_loop():
         #score&lives
         if pygame.sprite.spritecollide(phantomwolf,eggs,False):
             score+=1
+            collect.play()
         if n==1 and not pygame.sprite.spritecollide(phantomwolf,eggs,False):
             lives-=1
+            if lives!=2:
+                egg_cracking.play()
         if lives==2 or lives==3:
             screen.blit(lives_3,(390,475))
         if lives==1:
@@ -422,21 +604,26 @@ def game_loop():
         if lives==0:
             screen.blit(lives_1,(390,475))
         if lives==-1:
+            end_game=True
+            all_sprites.update()
+            all_sprites.draw(screen)
+            eggs.update()
+            end_game=False
+            highscores_help.append([score,name[10:]+'\n'])
+            game_over()
             i=0
             j=-1
             n=125
             score=0
             lives=3
-            skip=[0]
             first_pos=0
-            all_sprites.update()
-            all_sprites.draw(screen)
+            
             running=False
             
         all_sprites.draw(screen)
         
         eggs.draw(screen)
-        points(score)
+        points(score,(0,0,0),(440,7))
 
 
         pygame.display.update()
@@ -461,13 +648,64 @@ def game_about():
         if 260<mouse[0]<550 and 400<mouse[1]<460:
             pygame.draw.circle(screen,(0,0,0),(245,430),8)
             pygame.draw.circle(screen,(0,0,0),(560,430),8)
+            if beep_play==0:
+                beep.play()
+                beep_play=1
             if click[0]==1:
                 about=False
-
-        
-        
+        else:
+            beep_play=0
+                
         pygame.display.update()
 
+def game_highscores():
+
+    global highscores_help
+    beep_play=0
+    
+    running= True
+    while running:
+        screen.blit(menubackground,(0,0))
+        screen.blit(highscores_info,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+        highscores_text=open('highscores.txt')
+        highscores_display=highscores_text.readlines()
+        #print(highscores_display)
+
+
+        k=0
+        font = pygame.font.SysFont("comicsansms", 20)
+        for line in highscores_display:
+            text=font.render(line.rstrip("\n"), True, (0,0,0))
+            screen.blit(text,(285,120+k))
+            k+=25
+                
+        #pygame.draw.ellipse(screen,(0,0,0),(260,400,290,60))
+        screen.blit(back_button,(260,400))
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if 260<mouse[0]<550 and 400<mouse[1]<460:
+            pygame.draw.circle(screen,(0,0,0),(245,430),8)
+            pygame.draw.circle(screen,(0,0,0),(560,430),8)
+            if beep_play==0:
+                beep.play()
+                beep_play=1
+            if click[0]==1:
+                highscores_text.close()
+                running=False
+        else:
+            beep_play=0
+                
+        pygame.display.update()
+
+    
 
 
 game_intro()
